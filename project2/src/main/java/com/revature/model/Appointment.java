ecd3e93e008b348c87294233b50b09e3306e2f76
Keoffor <97791121+Keoffor@users.noreply.github.com>
2022-03-02 07:48:21 -0600

    added safe_merge to main for testing purposes

---
+++ project2/src/main/java/com/revature/model/Appointment.java
@@ -0,0 +1,37 @@
+package com.revature.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "appointment")
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+
+    //Josephs code included @manytoone before requesting_doctor and patient_for im not currently including it
+    @Column(name = "requesting_doctor")
+    private  int doctor;
+
+    @Column(name = "patient_for")
+    private int patient;
+
+    @Column(name = "schedule", nullable = false)
+    private String schedule;
+
+    @JsonFormat(pattern ="dd/mm/yyyy")
+    @Column(name = "time_of_appointment")
+    private Date appointmentTime;
+
+}
