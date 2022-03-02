8bb48cfef352f1df05a28be1cfe6ae02cd146831
sergiofrancovel <97748090+sergiofrancovel@users.noreply.github.com>
2022-03-02 09:23:32 -0600

    Delete project2 directory

--- project2/src/main/java/com/revature/dao/AppointmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.revature.dao;
-
-import com.revature.model.Appointment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
-}
